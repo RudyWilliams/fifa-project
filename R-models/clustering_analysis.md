ec355964f653a1a6c68d6307875afa074fbfde15
Rudy <rudysw05@knights.ucf.edu>
2019-07-04 00:13:25 -0400

    code for clustering on pca file. notes on clustering

---
+++ R-models/clustering_analysis.md
@@ -0,0 +1,6 @@
+## Clustering Analysis
+### K=3 on First Two PCs
+Three disitinct groups.
+  * The *green group* contains teams like Brazil, Portugal, Germany, Argentina, Italy, France, Croatia... good teams. Very good teams. Complete package type of teams. Good offense. Good defense.
+  * The *red group* contains teams like Ghana, Sengal, and East African nations (including Egypt)... well respected teams.
+  * The *blue group*.  The teams here stay pretty centered in terms of defense with the second principal component scores coming in between -3 and 3. Offense for these teams is average at best. The US/Canada (bc we grouped them together) are very separated offensively (PC1-wise) from the best offensive teams (Portugal, Brazil, Ivory Coast). Being on approximately the same horizontal line, defensive scores are comparable with Portugal and Brazil and better than Ivory Coast. US/Canada is in the range of favorable goalkeeping stats. This may be because of Tim Howard's star power.  
--- R-models/fifa_pca.R
+++ R-models/fifa_pca.R
@@ -2,7 +2,7 @@
 
 #load in the data
 library(readxl)
-data <-read_excel("C:/Users/rudyw/DataMining-S19/fifa19/means_feed_pca.xlsx")
+data <-read_excel("C:/Users/rudyw/DataMining-S19/fifa19/data/means_feed_pca.xlsx")
 dim(data)
 data.noids = data[,-1]
 ids = data[,1] 
@@ -28,3 +28,50 @@ biplot(pca, scale=T, xlim=c(-0.3,0), ylim=c(0.05,0.175))
 biplot(pca, scale=T, xlim=c(0,0.3), ylim=c(-0.25,-0.10))
 ##the goalkeeper stats
 ##
+
+
+#proportion of variance expained
+pcs.vars = pca$sdev^2
+pve = pcs.vars/sum(pcs.vars)
+pve
+
+plot(pve, type='b', xlab='Principal Component',
+     ylab='Proportion of Variance Explained')
+
+plot(c(0,cumsum(pve)), type='b', xlab='Principal Component',
+     ylab='Cumulative Proportion of Variance Explained')
+
+#just looking at the principal component scores for each team
+z1 = pca$x[,1]
+z2 = pca$x[,2]
+
+plot(z1, z2, xlab='First Principal Component', ylab='Second Principal Component',
+     type='n', xlim=c(-10,10), ylim=c(-10, 10))
+text(z1, z2, labels = seq(1,47))
+
+
+#k-means clustering on the PCs
+set.seed(5)
+k3.out = kmeans(pca$x[,c(1,2)], 3, nstart=200)
+plot(z1, z2, type='n',
+     main='K-Means Clustering on First Two Principal Components',
+     xlab='First Principal Component',
+     ylab='Second Principal Component')
+text(z1, z2, labels=seq(1,47), col=k3.out$cluster+1)
+
+k3.out
+
+#k-means on raw data
+set.seed(5)
+k3.raw = kmeans(scale(data.noids), 3, nstart=200)
+
+plot(z1, z2, type='n',
+     main='K-Means Clustering on Scaled Data',
+     sub='(Projected onto PCs for plotting)',
+     xlab='First Principal Component',
+     ylab='Second Principal Component')
+text(z1, z2, labels=seq(1,47), col=k3.raw$cluster+1)
+k3.raw
+
+table(true=k3.raw$cluster, predict=k3.out$cluster)
+
